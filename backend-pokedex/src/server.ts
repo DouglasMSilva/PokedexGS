2a94f330e6b3fe3e4639d69d457d3419f294cde6
DouglasMSilva <melosilvadouglas7@gmail.com>
2026-04-17 10:51:59 -0300

    Fixing routes

--- backend-pokedex/src/server.ts
+++ backend-pokedex/src/server.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import type { Request, Response } from 'express';
 import pokemonRoutes from './routes/pokemonRoutes';
-//import { connection } from './database/connection';
 
 const app = express();
 
@@ -10,30 +9,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-//app.get("/", async (req: Request, res: Response) => {
-   //try {
-  
-    
-
-    
-
-
-    //const result = await connection.execute("SELECT * FROM pokemons");
-
-    //const rows = result[0];
-    // é o mesmo que const [rows] = await connection.execute("SELECT * FROM pokemons");
-
-    //console.log(rows);
-
-    //res.json(rows);
-
-//    } catch (error) {
-//         res.status(500).json({ error });
-//    }
-//});
 
 // USANDO AS ROTAS
-app.use(pokemonRoutes);
+
+// ROTA TESTE (IMPORTANTE)
+app.get("/", (req: Request, res: Response) => {
+  res.send("API rodando");
+});
+
+app.use("/pokemons", pokemonRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
