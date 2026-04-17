a9fbbbf2ac3264dfec01fed5dbbdecefc5a85f0b
DouglasMSilva <melosilvadouglas7@gmail.com>
2026-04-17 11:33:28 -0300

    Fixing routes again

--- backend-pokedex/src/database/connection.ts
+++ backend-pokedex/src/database/connection.ts
@@ -6,5 +6,6 @@ export const connection = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT)
 });
 
